21b788da36b6ef2a034b455949e0aa306aca57e0
Denis Bakhoum <31353016+Dionysos5@users.noreply.github.com>
2022-08-16 09:59:18 +0000

    created navbar and added banner

--- front/src/App.js
+++ front/src/App.js
@@ -1,9 +1,18 @@
+import Navbar from "./components/Navbar";
 import Card from "./components/Card";
 
 function App() {
   return (
-    <div className="container  bg-gray-100 h-screen">
-      <Card />
+    <div className="bg-gray-100 w-full h-screen">
+      <Navbar />
+      <div className="flex justify-center bg-orange-400 items-center h-96 min-h-90 shadow-md mt-4 mb-4">
+        <h1 className="text-6xl text-center font-bold text-white uppercase"> Design your habits, create your future</h1>
+      </div >
+      <div className="grid grid-cols-5 gap-2">
+        <Card />
+        <Card />
+        <Card />
+      </div>
     </div>
   );
 }
