920725908e043547d79205508c2542be1e0bc0de
Denis Bakhoum <denis.bakhoum@phealing.fr>
2022-08-19 11:47:11 +0200

    Created initial navbar design

--- front/src/App.js
+++ front/src/App.js
@@ -1,8 +1,10 @@
 import Card from "./components/Card";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="container  bg-gray-100 h-screen">
+    <div className="">
+      <Navbar />
       <Card />
     </div>
   );
