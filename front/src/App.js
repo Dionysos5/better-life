fd706c1ff4e0c05071f329a32011c382f044e591
Denis Bakhoum <denis.bakhoum@phealing.fr>
2022-08-19 12:06:15 +0200

    Initial hero section design

--- front/src/App.js
+++ front/src/App.js
@@ -1,10 +1,12 @@
 import Card from "./components/Card";
+import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="">
       <Navbar />
+      <HeroSection />
       <Card />
     </div>
   );
