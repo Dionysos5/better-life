c9dfd67738c3b31a86cc31a91e99f28afb15450b
Denis Bakhoum <denis.bakhoum@phealing.fr>
2022-08-21 15:25:31 +0200

    Finished initial layout

--- front/src/App.js
+++ front/src/App.js
@@ -1,14 +1,12 @@
-import Card from "./components/Card";
 import Footer from "./components/Footer";
-import HeroSection from "./components/HeroSection";
+import Home from "./views/Home";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="">
       <Navbar />
-      <HeroSection />
-      <Card />
+      <Home />
       <Footer />
     </div>
   );
